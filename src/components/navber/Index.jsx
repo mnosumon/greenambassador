3daee1cef7dd0a2badecca7e1590c5da4c3472f3
mnosumon <mdnuruddin378350@gmail.com>
2024-11-21 15:13:31 +0600

    navbar logo setup done

--- src/components/navber/Index.jsx
+++ src/components/navber/Index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import HeaderLogo from "../../assets/image/logo-header.png";
 
 const Navbar = () => {
-  return <div>Index</div>;
+  return (
+    <nav>
+      <div className="w-[240px]">
+        <img className="w-full" alt="logo" src={HeaderLogo} />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
