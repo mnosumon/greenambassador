2aed669e1a5a81acce0f59d0283a837ca1e31b10
mnosumon <mdnuruddin378350@gmail.com>
2024-11-24 23:27:29 +0600

    Challange list components setup done

--- src/components/home/FeaturedChallenges.jsx
+++ src/components/home/FeaturedChallenges.jsx
@@ -3,32 +3,42 @@ import Heading from "../utilities/Heading";
 import Peragrap from "../utilities/Peragrap";
 import ChallangeCard from "../ChallangeCard";
 import PrimaryBTN from "../utilities/PrimaryBTN";
+import { useLocation } from "react-router-dom";
 
 const FeaturedChallenges = () => {
+  const location = useLocation();
   return (
     <section className="py-20">
       <div className="container mx-auto">
-        <Heading
-          className="!text-heading text-center !text-[52px]"
-          title="Featured Challenges"
-        />
-        <Peragrap
-          className="!text-heading text-center my-5"
-          title={
-            <>
-              Below you can find a selection of our educational challenges.
-              <br />
-              Read the educational briefing, take on the challenge and start
-              earning your rewards.
-            </>
-          }
-        />
+        {location.pathname === "/" ? (
+          <>
+            <Heading
+              className="!text-heading text-center !text-[52px]"
+              title="Featured Challenges"
+            />
+            <Peragrap
+              className="!text-heading text-center my-5"
+              title={
+                <>
+                  Below you can find a selection of our educational challenges.
+                  <br />
+                  Read the educational briefing, take on the challenge and start
+                  earning your rewards.
+                </>
+              }
+            />
+          </>
+        ) : (
+          ""
+        )}
         <div className="grid grid-cols-3  justify-between gap-8 py-10">
           <ChallangeCard />
         </div>
-        <div className="text-center py-5">
-          <PrimaryBTN title="explore all challange" />
-        </div>
+        {location.pathname === "/" && (
+          <div className="text-center py-5">
+            <PrimaryBTN title="explore all challange" />
+          </div>
+        )}
       </div>
     </section>
   );
--- src/peges/challange/Index.jsx
+++ src/peges/challange/Index.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ChallagneBanner from "../../components/challange/ChallagneBanner";
 import ChallangeListOverview from "../../components/challange/ChallangeListOverview";
+import ChallangCategory from "../../components/home/ChallangCategory";
+import FeaturedChallenges from "../../components/home/FeaturedChallenges";
 
 const Challange = () => {
   return (
     <div className="bg-[#F7F9FC]">
       <ChallagneBanner />
       <ChallangeListOverview />
+      <ChallangCategory />
+      <FeaturedChallenges />
     </div>
   );
 };
